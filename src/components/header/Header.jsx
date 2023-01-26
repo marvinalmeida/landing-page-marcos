d5143aa846ec497ffba2374c397cff2328840a7c
MARVIN SILVA DE ALMEIDA <marvin.c2a@tjpa.jus.br>
2023-01-26 18:11:45 -0300

    criação de menu e logo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import { Link } from "react-router-dom";
+import { Wrapper } from "./Header-styles";
+
+const Header = () => {
+  const { pathname } = window.location;
+
+  return (
+    <>
+      <Wrapper>
+        <div className="container">
+          <div className="logo">
+            <ImportContactsIcon />
+            <p>marcos</p>
+          </div>
+
+          <nav className="navegar">
+            <ul>
+              <li><Link className={pathname === "/" ? "active" : ""} to="/">home</Link></li>
+              <li><Link className={pathname === "Sobre" ? "active" : ""} to="#">sobre</Link></li>
+              <li><Link className={pathname === "/artigos" ? "active" : ""}to="/artigos">artigos</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Header;
