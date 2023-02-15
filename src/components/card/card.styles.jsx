a3a7c73d2e3771646c82caa518522b95b571eea4
Marvin Almeida <marvin.almeida31@gmail.com>
2023-02-15 09:46:33 -0300

    ajustes no mock e rotas por id

---
+++ src/components/card/card.styles.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .container {
+    margin: 20px 20px;
+    display: flex;
+    flex-direction: column;
+
+    -webkit-transition: -webkit-transform 0.5s ease;
+    transition: transform 0.5s ease;
+
+    :hover {
+      cursor: pointer;
+      -webkit-transform: scale(1.1);
+      transform: scale(1.1);
+    }
+  }
+
+  .img-name {
+    img {
+      max-width: 100%;
+      width: 200px;
+      margin-bottom: 10px;
+      height: 280px;
+      border-radius: 10px;
+    }
+    span {
+      width: 180px;
+      /* overflow: hidden !important;
+      text-overflow: ellipsis;
+      white-space: nowrap; */
+      margin-top: 5px;
+      margin-bottom: 10px;
+      font-size: 15px;
+      font-weight: bold;
+      text-transform: capitalize;
+    }
+    display: flex;
+    flex-direction: column;
+  }
+
+  .ano-duracao {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
